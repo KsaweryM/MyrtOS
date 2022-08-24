51e0b0f4865c9b87fb5e6a46e737e2dfce11e307
Ksawery Możdżyński <254624@student.pwr.edu.pl>
2022-08-24 21:46:31 +0200

    move definition of struct from .h file to .c file

--- Core/Inc/list.h
+++ Core/Inc/list.h
@@ -5,24 +5,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-typedef struct list_item
-{
-  struct list_item* next;
-  struct list_item* previous;
-  void* data;
-} list_item;
+typedef struct list_item list_item;
 
-typedef struct list
-{
-  list_item* list_begin;
-  list_item* list_end;
-} list;
+typedef struct list list;
 
-typedef struct iterator
-{
-  list_item* current_list_item;
-  list* iterator_owner;
-} iterator;
+typedef struct iterator iterator;
 
 list* list_create(void);
 
@@ -35,117 +22,13 @@ void list_push_back(list* list_object, void* data);
 iterator* iterator_create(list* list_object);
 void iterator_destroy(iterator* iterator_object);
 
+void* iteator_get_data(const iterator* iterator_object);
+void* iterator_pop(iterator* iterator_object);
+void iterator_reset(iterator* iterator_object);
 
-static inline void* iteator_get_data(const iterator* iterator_object);
-static inline void* iterator_pop(iterator* iterator_object);
-static inline void iterator_reset(iterator* iterator_object);
-static inline uint32_t iterator_next(iterator* iterator_object);
-static inline uint32_t iterator_previous(iterator* iterator_object);
-static inline uint32_t cyclic_iterator_next(iterator* iterator_object);
-static inline uint32_t cyclic_iterator_previous(iterator* iterator_object);
-
-static inline void* iteator_get_data(const iterator* iterator_object)
-{
-  if (iterator_object->current_list_item == 0)
-  {
-    return 0;
-  }
-
-  return iterator_object->current_list_item->data;
-}
-
-static inline void* iterator_pop(iterator* iterator_object)
-{
-  if (iterator_object->current_list_item == 0)
-  {
-    return 0;
-  }
-
-  list_item* current_list_item = iterator_object->current_list_item;
-  list_item* previous_list_item = current_list_item->previous;
-  list_item* next_list_item = current_list_item->next;
-
-  if (previous_list_item != 0 && next_list_item != 0)
-  {
-    previous_list_item->next = next_list_item;
-    next_list_item->previous = previous_list_item;
-  }
-  else if (previous_list_item == 0)
-  {
-    next_list_item->previous = 0;
-    iterator_object->iterator_owner->list_begin = next_list_item;
-  }
-  else if (next_list_item == 0)
-  {
-    previous_list_item->next = 0;
-    iterator_object->iterator_owner->list_end = previous_list_item;
-  }
-  else
-  {
-    iterator_object->iterator_owner->list_begin = 0;
-    iterator_object->iterator_owner->list_end = 0;
-  }
-
-  void* data = current_list_item->data;
-
-  iterator_object->current_list_item = next_list_item;
-
-  free(current_list_item);
-
-  return data;
-}
-
-static inline void iterator_reset(iterator* iterator_object)
-{
-  iterator_object->current_list_item = iterator_object->iterator_owner->list_begin;
-}
-
-static inline uint32_t iterator_next(iterator* iterator_object)
-{
-  if (iterator_object->current_list_item != 0)
-  {
-    iterator_object->current_list_item = iterator_object->current_list_item->next;
-  }
-
-  return iterator_object->current_list_item == 0;
-}
-
-
-static inline uint32_t iterator_previous(iterator* iterator_object)
-{
-  if (iterator_object->current_list_item != 0)
-  {
-    iterator_object->current_list_item = iterator_object->current_list_item->previous;
-  }
-
-  return iterator_object->current_list_item == 0;
-}
-
-static inline uint32_t cyclic_iterator_next(iterator* iterator_object)
-{
-  if (iterator_object->current_list_item == 0 || iterator_object->current_list_item->next == 0)
-  {
-    iterator_object->current_list_item = iterator_object->iterator_owner->list_begin;
-  }
-  else
-  {
-    iterator_object->current_list_item = iterator_object->current_list_item->next;
-  }
-
-  return iterator_object->current_list_item == 0;
-}
-static inline uint32_t cyclic_iterator_previous(iterator* iterator_object)
-{
-  if (iterator_object->current_list_item == 0 || iterator_object->current_list_item->previous == 0)
-  {
-    iterator_object->current_list_item = iterator_object->iterator_owner->list_end;
-  }
-  else
-  {
-    iterator_object->current_list_item = iterator_object->current_list_item->previous;
-  }
-
-  return iterator_object->current_list_item == 0;
-}
+uint32_t iterator_next(iterator* iterator_object);
+uint32_t iterator_previous(iterator* iterator_object);
+uint32_t cyclic_iterator_next(iterator* iterator_object);
+uint32_t cyclic_iterator_previous(iterator* iterator_object);
 
 #endif
--- Core/Src/list.c
+++ Core/Src/list.c
@@ -1,5 +1,24 @@
 #include "list.h"
 
+struct list_item
+{
+  struct list_item* next;
+  struct list_item* previous;
+  void* data;
+};
+
+struct list
+{
+  list_item* list_begin;
+  list_item* list_end;
+};
+
+struct iterator
+{
+  list_item* current_list_item;
+  list* iterator_owner;
+};
+
 list* list_create(void)
 {
   list* list_object = malloc(sizeof(*list_object));
@@ -81,4 +100,109 @@ iterator* iterator_create(list* list_object)
 void iterator_destroy(iterator* iterator_object)
 {
   free(iterator_object);
+}
+
+void* iteator_get_data(const iterator* iterator_object)
+{
+  if (iterator_object->current_list_item == 0)
+  {
+    return 0;
+  }
+
+  return iterator_object->current_list_item->data;
+}
+
+void* iterator_pop(iterator* iterator_object)
+{
+  if (iterator_object->current_list_item == 0)
+  {
+    return 0;
+  }
+
+  list_item* current_list_item = iterator_object->current_list_item;
+  list_item* previous_list_item = current_list_item->previous;
+  list_item* next_list_item = current_list_item->next;
+
+  if (previous_list_item != 0 && next_list_item != 0)
+  {
+    previous_list_item->next = next_list_item;
+    next_list_item->previous = previous_list_item;
+  }
+  else if (previous_list_item == 0)
+  {
+    next_list_item->previous = 0;
+    iterator_object->iterator_owner->list_begin = next_list_item;
+  }
+  else if (next_list_item == 0)
+  {
+    previous_list_item->next = 0;
+    iterator_object->iterator_owner->list_end = previous_list_item;
+  }
+  else
+  {
+    iterator_object->iterator_owner->list_begin = 0;
+    iterator_object->iterator_owner->list_end = 0;
+  }
+
+  void* data = current_list_item->data;
+
+  iterator_object->current_list_item = next_list_item;
+
+  free(current_list_item);
+
+  return data;
+}
+
+void iterator_reset(iterator* iterator_object)
+{
+  iterator_object->current_list_item = iterator_object->iterator_owner->list_begin;
+}
+
+uint32_t iterator_next(iterator* iterator_object)
+{
+  if (iterator_object->current_list_item != 0)
+  {
+    iterator_object->current_list_item = iterator_object->current_list_item->next;
+  }
+
+  return iterator_object->current_list_item == 0;
+}
+
+
+uint32_t iterator_previous(iterator* iterator_object)
+{
+  if (iterator_object->current_list_item != 0)
+  {
+    iterator_object->current_list_item = iterator_object->current_list_item->previous;
+  }
+
+  return iterator_object->current_list_item == 0;
+}
+
+uint32_t cyclic_iterator_next(iterator* iterator_object)
+{
+  if (iterator_object->current_list_item == 0 || iterator_object->current_list_item->next == 0)
+  {
+    iterator_object->current_list_item = iterator_object->iterator_owner->list_begin;
+  }
+  else
+  {
+    iterator_object->current_list_item = iterator_object->current_list_item->next;
+  }
+
+  return iterator_object->current_list_item == 0;
+}
+
+uint32_t cyclic_iterator_previous(iterator* iterator_object)
+{
+  if (iterator_object->current_list_item == 0 || iterator_object->current_list_item->previous == 0)
+  {
+    iterator_object->current_list_item = iterator_object->iterator_owner->list_end;
+  }
+  else
+  {
+    iterator_object->current_list_item = iterator_object->current_list_item->previous;
+  }
+
+  return iterator_object->current_list_item == 0;
 }
